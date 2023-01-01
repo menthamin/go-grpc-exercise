2d4023769bd07ad9c3bfe20cd8113985a63ca3b2
menthamin <menthamin@naver.com>
2023-01-01 23:26:09 +0900

    feat: add grpc example 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# References
+- dojinkim, "Golang gRPC server 구축하기 (1) - gRPC란 무엇인가?", https://devjin-blog.com/golang-grpc-server-1/
+- dojinkim, "dojinkim's GitHub", https://github.com/dojinkimm/go-grpc-example
---
+++ data/post.go
@@ -0,0 +1,76 @@
+// data/post.go
+package data
+
+import (
+	postpb "grpc-example/protos/v1/post"
+)
+
+type PostData struct {
+	UserID string
+	Posts  []*postpb.PostMessage
+}
+
+var UserPosts = []*PostData{
+	{
+		UserID: "1",
+		Posts: []*postpb.PostMessage{
+			{
+				PostId: "1",
+				Author: "", // Post 서비스는 자체적으로 유저의 이름은 알지 못한다
+				Title:  "gRPC 구축하기 (1)",
+				Body:   "gRPC 구축하려면 이렇게 하면 된다",
+				Tags:   []string{"gRPC", "Golang", "server", "coding", "protobuf"},
+			},
+			{
+				PostId: "2",
+				Author: "", // Post 서비스는 자체적으로 유저의 이름은 알지 못한다
+				Title:  "gRPC 구축하기 (2)",
+				Body:   "gRPC server란 이렇다",
+				Tags:   []string{"gRPC", "Golang", "server", "coding", "protobuf"},
+			},
+		},
+	},
+	{
+		UserID: "3",
+		Posts: []*postpb.PostMessage{
+			{
+				PostId: "5",
+				Author: "",
+				Title:  "2020.10.25 일기",
+				Body:   "오늘의 일기 끝",
+				Tags:   []string{"daily", "homework"},
+			},
+		},
+	},
+	{
+		UserID: "4",
+		Posts: []*postpb.PostMessage{
+			{
+				PostId: "6",
+				Author: "",
+				Title:  "은퇴하는 방법",
+				Body:   "은퇴하고 싶니?",
+				Tags:   []string{"retirement", "know-how"},
+			},
+			{
+				PostId: "7",
+				Author: "",
+				Title:  "저축하는 방법",
+				Body:   "저축하고 싶니?",
+				Tags:   []string{"money", "asset management", "know-how"},
+			},
+		},
+	},
+	{
+		UserID: "5",
+		Posts: []*postpb.PostMessage{
+			{
+				PostId: "8",
+				Author: "",
+				Title:  "회고하는 방법",
+				Body:   "좋은 리더는 이렇게 회고하더라",
+				Tags:   []string{"retrospect", "company", "teamwork", "leader"},
+			},
+		},
+	},
+}
